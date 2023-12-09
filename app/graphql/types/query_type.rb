c2bfa2ddb8c7a188edfec8370db45a9cbe8a786f
masatotezuka <tez.0731.mst@gmail.com>
2023-12-10 00:12:13 +0900

    feat:user query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,22 @@
 
 module Types
   class QueryType < Types::BaseObject
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+
+    field :users, [Types::UserType], null: false do
+    end
+
+    def users
+      User.all
+    end
+
+    
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
       argument :id, ID, required: true, description: "ID of the object."
     end
